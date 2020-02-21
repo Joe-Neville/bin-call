62d0c7cadbd29230d4ac9d575b14ef5ae3790799
Joe-Neville <joe.neville@hpe.com>
2020-02-21 20:31:40 +0000

    call test

--- bin.py
+++ bin.py
@@ -1,9 +1,16 @@
 import requests
 import pprint
+import pytest
 
 
 url = "https://httpbin.org/"
 
 r = requests.get(url + "get")
+
 print(f"Call Status Code: {r.status_code}")
 # print(f"Call Status Code: {r.status_code}")
+
+def test_response():
+    # url = "https://httpbin.org/"
+    # r = requests.get(url + "get")
+    assert r.status_code == 200
