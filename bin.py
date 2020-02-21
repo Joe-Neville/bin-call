584fd780118b8e0fca377befaf0fda3e7b6974a1
Joe-Neville <joe.neville@hpe.com>
2020-02-21 20:33:48 +0000

    line reduce

--- bin.py
+++ bin.py
@@ -6,11 +6,8 @@ import pytest
 url = "https://httpbin.org/"
 
 r = requests.get(url + "get")
-
 print(f"Call Status Code: {r.status_code}")
-# print(f"Call Status Code: {r.status_code}")
+
 
 def test_response():
-    # url = "https://httpbin.org/"
-    # r = requests.get(url + "get")
     assert r.status_code == 200
